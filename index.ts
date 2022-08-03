28cbc46ba92084a078329105c7d1e2ec1544dc49
Rico Fritzsche <rico@bluvolve.dev>
2022-08-03 17:13:42 +0200

    Config added

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from 'express';
+import {config} from "./config";
 
 const app: Express = express();
-const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Code with Rico. Ready to run on Heroku.');
 });
 
-app.listen(port, () => {
-    return console.log(`[server]: Server is running on ${port}`);
+app.listen(config.server.port, () => {
+    return console.log(`[server]: Server is running on ${config.server.port}`);
 });
